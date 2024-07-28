0d69af6f9b77e224907faa78b3afb39a818789db
Juni0r-player <vans-02@mail.ru>
2024-07-28 17:53:23 +0300

    Update lexicon_ru.py

--- lexicon/lexicon_ru.py
+++ lexicon/lexicon_ru.py
@@ -22,5 +22,9 @@ LEXICON_RU: dict[str, str] = {
     'bot_won': 'Я победил!\n\nСыграем еще?',
     'user_won': 'Ты победил! Поздравляю!\n\nДавай сыграем еще?',
     'nobody_won': 'Ничья!\n\nПродолжим?',
-    'bot_choice': 'Мой выбор'
+    'bot_choice': 'Мой выбор',
+    '/command_1': 'command_1 desription',
+    '/command_2': 'command_2 desription',
+    '/command_3': 'command_3 desription',
+    '/command_4': 'command_4 desription'
 }
