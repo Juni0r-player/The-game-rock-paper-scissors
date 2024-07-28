9b7558a7fd3ee287fc644299b352ff63fe41dd5e
Juni0r-player <vans-02@mail.ru>
2024-07-28 17:53:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from config_data.config import Config, load_config
 from handlers import other_handlers, user_handlers
+from keyboards.set_main_menu import set_main_menu
 
 logger = logging.getLogger(__name__)  # Инициализация логгера
 
@@ -35,6 +36,7 @@ async def main():
     dp.include_router(other_handlers.router)
 
     # Пропускаем накопившиеся апдейты и запускаем polling
+    await set_main_menu(bot)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
